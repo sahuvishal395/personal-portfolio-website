1c0cdde903bdf3d8fef36eca821fdbc208d7ffe6
vishalpersonal <sahuvishal395@gmail.com>
2026-04-08 20:07:19 +0530

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      hi  i am vishal product maketing lead
+      hi  i am vishal product manager lead
     </div>
   );
 }
